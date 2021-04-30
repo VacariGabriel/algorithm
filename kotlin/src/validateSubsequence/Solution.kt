5ce4d433fb1f45ec10fe9f694c3247c8f31edc01
Gabriel Vacari <gabriel_vacari@outlook.com>
2021-04-30 17:06:59 -0300

    create solution

--- kotlin/src/validateSubsequence/Solution.kt
+++ kotlin/src/validateSubsequence/Solution.kt
@@ -1,4 +1,17 @@
 package validateSubsequence
 
-class Solution {
+
+fun validateSubsequence(array: List<Int>, sequence: List<Int>): Boolean {
+    var arrayIndex = 0
+    var sequenceIndex = 0
+
+    while (arrayIndex < array.size && sequenceIndex < sequence.size) {
+        if (sequence[sequenceIndex] == array[arrayIndex]) {
+            sequenceIndex++
+        }
+        arrayIndex++
+    }
+
+    return sequenceIndex == sequence.size
 }
+
